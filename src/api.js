048a37ce68b6d93de5aa515438ac0f2939a7f022
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2022-12-25 19:20:35 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '63cea0b5a5msh59a5944add35bbdp134b71jsn376b3abddccc',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
